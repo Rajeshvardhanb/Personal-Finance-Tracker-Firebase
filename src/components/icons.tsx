889a8a8e190ecbab446465ac8dafe50764aa3450
Rajesh Vardhan Busam <busamrajeshvardhan@gmail.com>
2025-10-04 08:06:13 +0000

    I am seeing the unknow symbol. Can you please change it

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,12 +12,10 @@ export function AppLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M7 10h1v4" />
-      <path d="M16 10h-4v4h4" />
-      <path d="M12 10v4" />
-      <path d="M16 8.5a2.5 2.5 0 1 0-5 0" />
-      <path d="M5 6v12" />
-      <path d="M19 6v12" />
+      <path d="M6 3h12" />
+      <path d="M6 8h12" />
+      <path d="M18 8a6 6 0 0 1-12 0" />
+      <path d="M12 2v20" />
     </svg>
   );
 }
