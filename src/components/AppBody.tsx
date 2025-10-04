ae415d988b961f5c7e4b91846f7bb5fed0ad25cd
Rajesh Vardhan Busam <busamrajeshvardhan@gmail.com>
2025-10-04 11:19:52 +0000

    still same. First of all Have you understood the issue?

--- src/components/AppBody.tsx
+++ src/components/AppBody.tsx
@@ -3,15 +3,21 @@
 import { useSidebar } from '@/hooks/use-sidebar-provider';
 import Header from '@/components/Header';
 import AppSidebar from '@/components/AppSidebar';
+import { cn } from '@/lib/utils';
 
 export default function AppBody({ children }: { children: React.ReactNode }) {
   const { state } = useSidebar();
   return (
-    <div className="relative flex min-h-screen w-full flex-col" data-sidebar={state}>
+    <div className="relative flex min-h-screen w-full flex-col">
       <Header />
       <div className="flex flex-1">
         <AppSidebar />
-        <main className="flex-1 p-4 transition-all duration-300 ease-in-out data-[sidebar=expanded]:sm:ml-72 sm:px-6 sm:py-4 sm:ml-[78px]">
+        <main
+          className={cn(
+            'flex-1 p-4 transition-all duration-300 ease-in-out sm:px-6 sm:py-4',
+            state === 'expanded' ? 'sm:ml-72' : 'sm:ml-[78px]'
+          )}
+        >
           {children}
         </main>
       </div>
