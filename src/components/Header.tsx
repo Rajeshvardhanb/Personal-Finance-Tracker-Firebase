6756b8c3ffe82aca3a6ba6f209a3e64cc2ea6ea6
Rajesh Vardhan Busam <busamrajeshvardhan@gmail.com>
2025-10-04 08:39:46 +0000

    side bar is over lapping main screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 sm:ml-64">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <Sheet>
         <SheetTrigger asChild>
           <Button size="icon" variant="outline" className="sm:hidden">
@@ -55,13 +55,15 @@ export default function Header() {
         </SheetTrigger>
         <SheetContent side="left" className="sm:max-w-xs">
           <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="/"
-              className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-            >
-              <AppLogo className="h-5 w-5 transition-all group-hover:scale-110" />
-              <span className="sr-only">Personal Finance Tracker</span>
-            </Link>
+            <SheetClose asChild>
+              <Link
+                href="/"
+                className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+              >
+                <AppLogo className="h-5 w-5 transition-all group-hover:scale-110" />
+                <span className="sr-only">Personal Finance Tracker</span>
+              </Link>
+            </SheetClose>
             {navItems.map((item) => (
               <SheetClose asChild key={item.href}>
                 <Link
@@ -76,6 +78,15 @@ export default function Header() {
                 </Link>
               </SheetClose>
             ))}
+             <SheetClose asChild>
+                <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                    <Settings className="h-5 w-5" />
+                    Settings
+                </Link>
+            </SheetClose>
           </nav>
         </SheetContent>
       </Sheet>
