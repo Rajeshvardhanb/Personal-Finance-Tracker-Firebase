e819862e567348f2ba59f2e62ec73b58e6863a14
Rajesh Vardhan Busam <busamrajeshvardhan@gmail.com>
2025-10-04 12:06:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2885772660-ed7a7",
+  "appId": "1:249222121107:web:5b70ac8e83ceb4c8ff2f66",
+  "apiKey": "AIzaSyDODEZDf26BvflMuVvdTe31_Op1av5G8l0",
+  "authDomain": "studio-2885772660-ed7a7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "249222121107"
+};
